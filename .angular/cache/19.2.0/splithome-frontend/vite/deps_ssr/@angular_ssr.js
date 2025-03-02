42f2262d90b01b77d1d2e88310a2c73414e63abf
WendelDoa <sswendel11@outlook.com>
2025-03-02 18:28:52 -0300

    Reconstrução do projeto sem SSR

---
+++ .angular/cache/19.2.0/splithome-frontend/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,39 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell
+} from "./chunk-HTJPPOFX.js";
+import "./chunk-D7LRIO7K.js";
+import "./chunk-SX6IWW4I.js";
+import "./chunk-WOVMO7QA.js";
+import "./chunk-ULIMLLNR.js";
+import "./chunk-Q2625NID.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  withAppShell,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
-import { UserlistComponent } from './components/userlist/userlist.component';
 
-export const routes: Routes = [
-    {path: "userlist", component: UserlistComponent}
-];
+export const routes: Routes = [];
