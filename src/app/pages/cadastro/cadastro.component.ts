92e63e254c486e931e95fc4bfdc69a7bb932de49
natan <cesardasilvanataniel@gmail.com>
2025-03-20 16:16:08 -0300

    fix #6:  refactor component cadastro

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,33 +1,63 @@
 import { Component, inject, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { Router } from '@angular/router';
-import { MatIcon } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { UserService } from '../../core/auth/user/user.service';
 import { Register } from '../../core/auth/user/register';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { CommonModule } from '@angular/common';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [MatCardModule, MatButtonModule, MatIcon, FormsModule, MatFormFieldModule, MatInputModule],
+  standalone: true,
+  imports: [
+    MatCardModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    FormsModule, 
+    ReactiveFormsModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    CommonModule,
+
+  ],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.scss'
+  styleUrls: ['./cadastro.component.scss']
 })
 export class CadastroComponent {
 
-  name!: string;
-  email!: string;
-  password!: string;
-  repeatPassword!: string;
-
+  cadastroForm: FormGroup;
   router = inject(Router);
-  private _snackBar = inject(MatSnackBar);
   userService = inject(UserService);
   hide1 = signal(true);
   hide2 = signal(true);
+
+  constructor(private fb: FormBuilder) {
+    this.cadastroForm = this.fb.group(
+      {
+        name: ['', [Validators.required, Validators.maxLength(20)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/[!@#$%^&*(),.?":{}|<>]/)]],
+        repeatPassword: ['', [Validators.required], [this.passwordMatchValidator.bind(this)]]      }
+    );
+  }
+
+  passwordMatchValidator(control: AbstractControl): Promise<ValidationErrors | null> {
+    return new Promise((resolve) => {
+      const password = this.cadastroForm.get('password')?.value;
+      const repeatPassword = control.value;
+
+      if (password !== repeatPassword) {
+        resolve({ passwordMismatch: true });
+      } else {
+        resolve(null);
+      }
+    });
+  }
 
   clickEventPassword(event: MouseEvent) {
     this.hide1.set(!this.hide1());
@@ -40,22 +70,27 @@ export class CadastroComponent {
   }
 
   registrar() {
-    if (this.password != this.repeatPassword) {
-      this.openSnackBar("As senhas não coincidem!")
-    } else {
-      const register: Register = new Register(this.name, this.email, this.password);
-      this.userService.cadastrar(register).subscribe({
-        next: string => {
-          this.openSnackBar("Usuário cadastrado com sucesso!");
-          this.router.navigate(["/login"])
-        }, error: erro => {
-          alert("Algo saiu errado!")
-        }
-      })
+    if (this.cadastroForm.invalid) {
+      return;
     }
+
+    const { name, email, password } = this.cadastroForm.value;
+    const register: Register = new Register(name, email, password);
+    
+    this.userService.cadastrar(register).subscribe({
+      next: () => {
+        this.router.navigate(["/login"]);
+      },
+      error: (error) => {
+        if (error.status === 409) {
+          const errorMessage = JSON.parse(error.error)?.message;
+          alert(errorMessage); 
+        } else {
+          alert("Tivemos um erro interno, lamentamos.");
+          console.error('Erro interno', error);
+        }
+      }
+    });
   }
 
-  openSnackBar(message: string) {
-    this._snackBar.open(message, '', { duration: 5000 });
-  }
 }
