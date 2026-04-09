e1a9780b53269d0798f9755ef7e5442d266355ea
Wendel Sampaio <111626474+Wendel-Sampaio@users.noreply.github.com>
2026-04-08 21:46:13 -0300

    refactor: move despesas component into home shared components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,10 @@ import { loginGuard } from './core/auth/guards/login.guard';
 import { homeGuard } from './core/auth/guards/home.guard';
 
 export const routes: Routes = [
-    
-    {path: "", redirectTo: "login", pathMatch: "full"},
-    {path: "login", component: LoginComponent, canActivate: [homeGuard]},
-    {path: "cadastro", component: CadastroComponent, canActivate: [homeGuard]},
-    {path: "home", component: HomeComponent, canActivate: [loginGuard]}
+
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent, canActivate: [homeGuard] },
+    { path: 'cadastro', component: CadastroComponent, canActivate: [homeGuard] },
+    { path: 'home', component: HomeComponent, canActivate: [loginGuard] },
+    { path: '**', redirectTo: 'home' }
 ];
