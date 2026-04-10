769e2330c3df055cb16591694651ec89795c8f4f
Wendel-Sampaio <sswendel11@outlook.com>
2026-04-10 01:41:16 -0300

    Add: Component despesa

--- src/app/shared/components/despesas/despesas.component.ts
+++ src/app/shared/components/despesas/despesas.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardTitle } from "@angular/material/card";
+import { MatDialog, MatDialogModule } from "@angular/material/dialog";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTableModule } from "@angular/material/table";
+import { FormCompraComponent } from "../form-compra/form-compra.component";
 
 @Component({
   selector: 'app-despesas',
-  imports: [],
+  imports: [
+    MatTableModule,
+    CommonModule,
+    MatDialogModule,
+    MatCardTitle,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './despesas.component.html',
   styleUrl: './despesas.component.scss'
 })
 export class DespesasComponent {
 
+  readonly dialog = inject(MatDialog);
+  
+  abrirFormCompra() {
+    const formRef = this.dialog.open(FormCompraComponent, {
+      width: '550px',
+    });
+    formRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+      //this.pegarCompras();
+    });
+  }  
+  
+  
 }
